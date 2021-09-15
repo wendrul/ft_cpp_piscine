ab715374a27f37be733e0f650e0d05b101a921b3
wendrul <dethomassin.etienne@gmail.com>
2021-09-15 12:17:33 +0200

    Finish sed

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,26 +2,52 @@
 #include <fstream>
 #include <sstream>
 
-
-// void replace(std::string needle, std::string replacementNeedle, std::string filename)
-// {
-//     (void)needle
-// }
-
-int main(int argc , char **argv)
+void sed(std::string fileName, std::string needle, std::string newNeedle)
 {
-    std::ostringstream  text;
-    std::string filename(argv[1]);
-    std::ifstream inputFile(argv[1]);
-    std::ofstream outputFile;
+    std::ifstream file(fileName);
+    if (!file)
+    {
+        std::cerr << "Couldn't open " << fileName << std::endl;
+        return;
+    }
+    std::ostringstream text;
+    text << file.rdbuf();
+    std::string s = text.str();
+    size_t index;
+    
+    while (1)
+    {
+        index = s.find(needle);
+        if (index == std::string::npos)
+            break;
+        s.erase(index, needle.length());
+        s.insert(index, newNeedle);
+    }
+    file.close();
+    
+    std::ofstream ret(fileName + ".replace");
+    ret << s;
+}
+
+int main(int argc, char **argv)
+{
     if (argc != 4)
     {
-        std::cerr << "Wrong number of arguments.\n";    
+        std::cerr << "Wrong number of arguments.\n";
         return 0;
     }
-    inputFile.open(filename);
-    outputFile.open(filename + ".replace");
-    text << inputFile.rdbuf();
-    outputFile << text << std::endl;
+
+    std::string fileName(argv[1]);
+    std::string needle(argv[2]);
+    std::string newNeedle(argv[3]);
+    
+    if (fileName.length() == 0 || needle.length() == 0 || newNeedle.length() == 0)
+    {
+        std::cerr << "Invalid or empty parameters" << std::endl;
+        return (0);
+    }
+    
+    sed(fileName, needle, newNeedle);
+    
     return 0;
 }
