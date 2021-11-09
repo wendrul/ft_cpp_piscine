8c5e44b7a24861b9cfbe56e7be2359315a219e6e
wendrul <dethomassin.etienne@gmail.com>
2021-11-09 11:12:27 +0100

    Update main with subject tests

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -19,8 +19,21 @@ int main()
 
     std::cout << "explicit template\n";
     std::cout << "max of ints: " << max<int>(a_i, b_i) << std::endl;
-    std::cout << "max of floats (implictly casted to ints): " << max<int>(a_f, b_f) << std::endl;
+    std::cout << "max of floats (implictly casted to ints): " << max<int>(a_f, b_f) << "\n\n\n";
 
+    std::cout << "Subject test main\n\n";
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 
     return 0;
 }
