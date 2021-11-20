3446a84eb679623fd75ee3630d91f4dd11405fad
wendrul <dethomassin.etienne@gmail.com>
2021-11-20 19:33:38 +0100

    cannonical and exception message

--- cpp08/ex01/span.hpp
+++ cpp08/ex01/span.hpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <functional>
 
-#define SPAN_FULL "Span is full"
+#define SPAN_FULL "Span is already full"
 #define NOT_ENOUGH_SPACE_TO_ADD_VECT "Not enough space left to add this vect"
 #define NOT_ENOUGH_VALUES_FOR_SPAN "There are not enough values to form a span (min 2)"
 
@@ -16,7 +16,7 @@ class Span
 public:
     Span(unsigned int maxSize);
     Span(const Span &that);
-    ~Span();
+    virtual ~Span();
     Span &operator=(const Span &that);
 
     void addNumber(const int newNumber);
